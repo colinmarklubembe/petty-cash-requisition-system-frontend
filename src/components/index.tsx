b9ec44ee01a7820e07506705e23f20c86958d865
colinmarklubembe <107845562+colinmarklubembe@users.noreply.github.com>
2024-08-11 16:47:02 +0300

    refactor: Update component imports in index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,35 +1,35 @@
 // ladning page
-export { default as HeaderSection } from "./Header";
+export { default as Footer } from "./Footer";
 export { default as HeroSection } from "./HeroSection";
-export { default as FeaturesSection } from "./FeaturesSection";
 export { default as Testimonials } from "./Testimonials";
 export { default as CallToAction } from "./CallToAction";
-export { default as Footer } from "./Footer";
+export { default as HeaderSection } from "./Header";
+export { default as FeaturesSection } from "./FeaturesSection";
 // ui
 export { default as Modal } from "./ui/Modal";
 export { default as Table } from "./ui/Table";
 export { default as Header } from "./ui/Header";
 export { default as Sidebar } from "./ui/SideBar";
+export { default as StatCard } from "./ui/StatCard";
+export { default as ChartCard } from "./ui/ChartCard";
 export { default as ActionsMenu } from "./ui/ActionsMenu";
 export { default as LoadingSpinner } from "./ui/LoadingSpinner";
 export { default as DownloadButton } from "./ui/DownloadReport";
-export { default as SessionExpiredDialog } from "./ui/SessionExpiredDialog";
 export { default as ConfirmDeleteDialog } from "./ui/ConfirmDelete";
-export { default as ChartCard } from "./ui/ChartCard";
-export { default as StatCard } from "./ui/StatCard";
+export { default as SessionExpiredDialog } from "./ui/SessionExpiredDialog";
 // forms
+export { default as InviteUser } from "./forms/InviteUser";
+export { default as EditCompany } from "./forms/EditCompany";
 export { default as CreateCompany } from "./forms/CreateCompany";
+export { default as EditPettyFundForm } from "./forms/EditPettyFund";
 export { default as CreateTransaction } from "./forms/CreateTransaction";
 export { default as CreateRequisition } from "./forms/CreateRequisition";
-export { default as CreatePettyFundForm } from "./forms/CreatePettyFundForm";
-export { default as EditPettyFundForm } from "./forms/EditPettyFund";
 export { default as EditRequisitionForm } from "./forms/EditRequisition";
-export { default as EditCompany } from "./forms/EditCompany";
-export { default as InviteUser } from "./forms/InviteUser";
+export { default as CreatePettyFundForm } from "./forms/CreatePettyFundForm";
 // views
-export { default as TransactionDetailsView } from "./views/Transaction";
-export { default as RequisitionDetailsView } from "./views/Requisition";
-export { default as PettyFundDetailView } from "./views/PettyFund";
-export { default as CompanyReportView } from "./views/CompanyReportView";
 export { default as UserReportView } from "./views/UserReportView";
 export { default as UserDetailView } from "./views/UserDetailView";
+export { default as CompanyReportView } from "./views/CompanyReportView";
+export { default as PettyFundDetailView } from "./views/PettyFund";
+export { default as TransactionDetailsView } from "./views/Transaction";
+export { default as RequisitionDetailsView } from "./views/Requisition";
